ce35a6022c1e083f2584da9acb737c239d09e79b
09130510 <09130510@url.com.tw>
2017-02-09 10:40:53 +0800

    2017/02/09 Init Upload

---
+++ APIs/Speedy/Dll/Reg.bat
@@ -0,0 +1,4 @@
+regsvr32 -u c:\windows\system32\OrderConnection.dll
+regsvr32 -u c:\windows\system32\OrderMessage.dll
+regsvr32 c:\windows\system32\OrderConnection.dll
+regsvr32 c:\windows\system32\OrderMessage.dll
---
+++ APIs/Speedy/Dll2/reg.bat
@@ -0,0 +1,2 @@
+regsvr32 c:\windows\system32\OrderConnection.dll
+regsvr32 c:\windows\system32\OrderMessage.dll
